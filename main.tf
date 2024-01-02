f2342600f89f5fedbe7d772c9e501718771521b4
Waqas Muazam <116129002+wajas86@users.noreply.github.com>
2024-01-02 15:28:35 +0000

    RDS_Instance_Configuration

---
+++ main.tf
@@ -0,0 +1,39 @@
+provider "aws" {
+  access_key = var.access_key
+  secret_key = var.secret_key
+  region = eu-west-2
+}
+
+resource "aws_db_instance" "caci_database" {
+  identifier = "caci_rds_instance"
+  engine = "mysql"
+  instance_class = "db.t2.micro"
+  allocated_storage = "20"
+  storage_type = "gp2"
+  availability_zone = "eu-west-2"
+  multi_az = true
+  publicly_accessible = false
+}
+
+
+resource "aws_security_group" "caci_db_security_group" {
+  name        = "db-security-group-caci"
+  description = "Security group for caci RDS instance"
+
+  ingress {
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    cidr_blocks = ["116.30.45.50/32"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["116.30.45.50/32"]
+  }
+}
+
+
+
---
+++ variables.tf
@@ -0,0 +1,7 @@
+variable "access_key" {
+  default = "AKIAZTULVTA376BRMDFF"
+}
+
+variable "secret_key" {
+  default = "cD550iCJjzG06C5JdhelBYp8aXgNIqG465UQlSOw"
+}
